8abcaa0714d0caf384e347fb9e5508272929268a
GioPan04 <gioele@pannetto.com>
2023-04-16 14:01:42 +0200

    Reconnect when player is destroyed

--- src/player_thread.rs
+++ src/player_thread.rs
@@ -1,18 +1,39 @@
+use mpris::Player;
 use mpris::PlayerFinder;
 use mpris::Event;
+use std::time::Duration;
+use std::{time, thread};
+
+const RETRY_DURATION: Duration = time::Duration::from_secs(3);
 
 pub fn player_runner() {
-  let finder = PlayerFinder::new().unwrap();
+  let connection = PlayerFinder::new().expect("Cannot find a D-Bus connection. Killing");
+  
+  loop {
+    let mut player = connection.find_active();
+  
+    while let Err(_) = player {
+      thread::sleep(RETRY_DURATION);
+      player = connection.find_active();
+    }
+    
+    handle_player(player.unwrap());
+    println!("Disconnected from player");
+  }
+}
 
-  let player = finder.find_active().unwrap();
+
+fn handle_player(player: Player) {
+  println!("Connected to player");
 
   let events = player.events().unwrap();
 
   for event in events {
-    match event.unwrap() {
-      Event::Paused => println!("Paused"),
-      Event::Playing => println!("Playing"),
-      Event::TrackChanged(track) => println!("{:?}", track),
+    match event {
+      Ok(Event::Paused) => println!("Paused"),
+      Ok(Event::Playing) => println!("Playing"),
+      Ok(Event::TrackChanged(track)) => println!("{:?}", track),
+      Err(_) => return,
       _ => ()
     }
   }
